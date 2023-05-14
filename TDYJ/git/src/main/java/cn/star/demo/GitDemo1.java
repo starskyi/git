886cda008667ae2bd5a44860c66ffb3d9ce508c5
star <star@sky.com>
2023-05-14 15:44:58 +0800

    Git first commit

---
+++ TDYJ/git/src/main/java/cn/star/demo/GitDemo1.java
@@ -0,0 +1,9 @@
+package cn.star.demo;
+
+public class GitDemo1 {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+    }
+}
