d6c99423e3b7a88c5bf94cc57051e0686c6d9c3d
star <star@sky.com>
2023-05-14 15:49:38 +0800

    Git second commit

--- TDYJ/git/src/main/java/cn/star/demo/GitDemo1.java
+++ TDYJ/git/src/main/java/cn/star/demo/GitDemo1.java
@@ -5,5 +5,6 @@ public class GitDemo1 {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("master hello world");
     }
 }
