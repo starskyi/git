9822fd813520f28a02c52aa5966e592eddddbc2f
star <star@sky.com>
2023-05-14 15:47:01 +0800

    Git hot-fix first commit

--- TDYJ/git/src/main/java/cn/star/demo/GitDemo1.java
+++ TDYJ/git/src/main/java/cn/star/demo/GitDemo1.java
@@ -4,6 +4,6 @@ public class GitDemo1 {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello world");
-
+        System.out.println("hello world");
     }
 }
