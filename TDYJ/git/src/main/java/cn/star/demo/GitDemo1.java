1f4879a860484726e6f42d63ce5388641c10a6fd
starskyi <133491679+starskyi@users.noreply.github.com>
2023-05-14 16:12:24 +0800

    Update GitDemo1.java

--- TDYJ/git/src/main/java/cn/star/demo/GitDemo1.java
+++ TDYJ/git/src/main/java/cn/star/demo/GitDemo1.java
@@ -1,11 +1,1 @@
-package cn.star.demo;
 
-public class GitDemo1 {
-    public static void main(String[] args){
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("master hello world");
-    }
-}
